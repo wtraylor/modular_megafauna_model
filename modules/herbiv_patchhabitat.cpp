b4b0b039f9aa4fc0213701fa72d8d4e783d835a0
Wolfgang Pappa <wolfgang.pappa@posteo.de>
2017-11-07 13:59:28 +0100

    Temporary fix: disable FPC from LPJ-GUESS.
    There were irregular FPC values that didn’t fit
    the grass biomass. I set it to 0.5 constant. This
    is just a dirty fix.

--- modules/herbiv_patchhabitat.cpp
+++ modules/herbiv_patchhabitat.cpp
@@ -78,29 +78,30 @@ HabitatForage PatchHabitat::get_available_forage() const {
 			//------------------------------------------------------------
 			// GRASS SPECIFIC
 			if (ft == FT_GRASS){
-				if (indiv_mass > 0.0) {
 
-					// Maximum reasonable grass sward density [kgDM/km²].
-					static const double MAX_SWARD_DENS = 20 * 10000; // = 20 kg/m²
-
-					if (indiv.fpc <= 0.0)
-						throw std::logic_error(
-								"Fauna::PatchHabitat::get_available_forage() "
-								"The grass individual has positive leaf biomass "
-								"but its FPC is zero or negative.");
-
-					if (indiv_mass/indiv.fpc > MAX_SWARD_DENS)
-						throw std::logic_error(
-								"Fauna::PatchHabitat::get_available_forage() "
-								"The grass individual has positive leaf biomass "
-								"but its FPC is near zero. "
-								"This results in a preposterously high grass sward "
-								"density.");
-
-					// Build sum of FPCs
-					// We assume there is never any overlap of grass PFTs.
-					grass_fpc += indiv.fpc;
-				}
+				// if (indiv_mass > 0.0) {
+        //
+				// 	// Maximum reasonable grass sward density [kgDM/km²].
+				// 	static const double MAX_SWARD_DENS = 20 * 10000; // = 20 kg/m²
+        //
+				// 	if (indiv.fpc <= 0.0)
+				// 		throw std::logic_error(
+				// 				"Fauna::PatchHabitat::get_available_forage() "
+				// 				"The grass individual has positive leaf biomass "
+				// 				"but its FPC is zero or negative.");
+        //
+				// 	if (indiv_mass/indiv.fpc > MAX_SWARD_DENS)
+				// 		throw std::logic_error(
+				// 				"Fauna::PatchHabitat::get_available_forage() "
+				// 				"The grass individual has positive leaf biomass "
+				// 				"but its FPC is near zero. "
+				// 				"This results in a preposterously high grass sward "
+				// 				"density.");
+        //
+				// 	// Build sum of FPCs
+				// 	// We assume there is never any overlap of grass PFTs.
+				// 	grass_fpc += indiv.fpc;
+				// }
 			}
 
 			//------------------------------------------------------------
@@ -108,7 +109,19 @@ HabitatForage PatchHabitat::get_available_forage() const {
 
 		}
 	}
-	result.grass.set_fpc( grass_fpc );
+  // TODO: Grass FPC is made constant because 
+  // LPJ-GUESS produced near-zero FPC values.
+  if (result.grass.get_mass() > 0.0) {
+    grass_fpc = 0.5; // DIRTY FIX CONSTANT
+  } else
+    grass_fpc = 0.0;
+
+  // Keep FPC in valid range. If LPJ-GUESS has unreasonable values, we just
+  // correct them. It’s not our issue to complain about errors in the
+  // vegetation model here.
+  grass_fpc = max(grass_fpc, 0.0);
+  grass_fpc = min(grass_fpc, 1.0);
+  result.grass.set_fpc( grass_fpc );
 
 	return result;
 }
