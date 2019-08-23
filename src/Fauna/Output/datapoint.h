ab5f07fa9037b701226107aa681165ed76e16ba4
Wolfgang Traylor <wolfgang.traylor@senckenberg.de>
2019-08-23 13:41:44 +0200

    Add struct Fauna::Output::Datapoint

---
+++ src/Fauna/Output/datapoint.h
@@ -0,0 +1,39 @@
+#ifndef DATA_POINT_H
+#define DATA_POINT_H
+
+#include "combined_data.h"
+#include "date.h"
+#include "parameters.h"
+
+namespace Fauna {
+namespace Output {
+
+/// Simple container for spatially and temporally aggregated output data.
+/**
+ * \ref CombinedData already holds and aggregates output data, but this class
+ * additionally contains the relevant metadata for *what* the data represents.
+ */
+struct Datapoint {
+  /// Identifier of the spatial aggregation unit.
+  /**
+   * \see Fauna::Habitat::get_aggregation_unit()
+   */
+  std::string aggregation_unit;
+
+  /// The aggregated output data itself.
+  CombinedData data;
+
+  /// First day of the aggregated time interval.
+  Date first_day;
+
+  /// The length of the aggregated time interval.
+  OutputInterval interval;
+
+  /// Last day of the aggregated time interval.
+  Date last_day;
+};
+
+}  // namespace Output
+}  // namespace Fauna
+
+#endif  // DATA_POINT_H
