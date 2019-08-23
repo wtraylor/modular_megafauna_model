1776a3b362b6cd3f77ca76002a2a8ca6c6b8257e
Wolfgang Traylor <wolfgang.traylor@senckenberg.de>
2019-08-23 13:32:52 +0200

    Implement functions of class Date

--- src/Fauna/date.cpp
+++ src/Fauna/date.cpp
@@ -1,13 +1,24 @@
 #include "date.h"
+#include <stdexcept>
 
 using namespace Fauna;
 
 Date::Date(const unsigned int julian_day, const int year)
     : julian_day(julian_day), year(year) {
-  // TODO
+  if (julian_day < 0 || julian_day > 365)
+    throw std::invalid_argument(
+        "Fauna::Date::Date() The parameter `julian_day` is out of range.");
 }
 
 bool Date::is_successive(const Date& other_date) const {
-  // TODO
-  return true;
+  // One day follows within one year.
+  if ((other_date.get_year() == this->get_year()) &&
+      (other_date.get_julian_day() == this->get_julian_day() + 1))
+    return true;
+  // We step from December 31st to January 1st.
+  if ((other_date.get_year() == this->get_year() + 1) &&
+      (other_date.get_julian_day() == 0) &&
+      (this->get_julian_day() == 364 || this->get_julian_day() == 365))
+    return true;
+  return false;
 }
