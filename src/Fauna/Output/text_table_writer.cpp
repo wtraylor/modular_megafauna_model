51e6f8086302658d1a989b22362a93d115e8cc91
Wolfgang Traylor <wolfgang.traylor@posteo.de>
2019-08-24 17:37:11 +0200

    Start implementing TextTableWriter

--- src/Fauna/Output/text_table_writer.cpp
+++ src/Fauna/Output/text_table_writer.cpp
@@ -9,6 +9,92 @@ const char* TextTableWriter::FILE_EXTENSION = ".tsv";
 TextTableWriter::TextTableWriter(
     const OutputInterval interval,
     const Parameters::TextTableWriterOptions& options)
-    : interval(interval), options(options) {}
+    : interval(interval), options(options) {
+  const std::string& dir = options.output_directory;
 
-void TextTableWriter::write_datapoint(const Datapoint& datapoint) {}
+  // Add all selected output files to list of file streams.
+  if (options.mass_density_per_hft){
+    file_streams.push_back(&mass_density_per_hft);
+    mass_density_per_hft.open(dir + "mass_density_per_hft");
+  }
+  // -> Add new output files here.
+
+  // TODO: create directory & empty files
+}
+
+void TextTableWriter::write_datapoint(const Datapoint& datapoint) {
+  const CombinedData& data = datapoint.data;
+
+  if (!captions_written) {
+    write_captions(datapoint);
+    captions_written = true;
+  }
+
+  // Write common information for all output tables.
+  for (auto& f : file_streams) {
+    switch (interval) {
+      case OI_DAILY:
+        // TODO
+        break;
+      case OI_MONTHLY:
+        // TODO
+        break;
+      case OI_ANNUAL:
+        *f << datapoint.interval.get_first().get_year() << FIELD_SEPARATOR;
+        break;
+      case OI_DECADAL:
+        // TODO
+        break;
+      default:
+        std::logic_error(
+            "Fauna::TextTableWriter::write_datapoint() Output time interval is "
+            "not implemented.");
+    }
+    // TODO: Check that aggregation unit has no field separator in it.
+    *f << datapoint.aggregation_unit << FIELD_SEPARATOR;
+  }
+
+  // Per HFT Tables
+  // TODO: To what precision will be rounded?
+  for (const auto i : datapoint.data.hft_data){
+    if (mass_density_per_hft.is_open())
+      mass_density_per_hft << i.second.massdens;
+  }
+
+}
+
+void TextTableWriter::write_captions(const Datapoint& datapoint) {
+  // Write common column captions for all output tables.
+  for (auto& f : file_streams) {
+    switch (interval) {
+      case OI_DAILY:
+        // TODO
+        break;
+      case OI_MONTHLY:
+        // TODO
+        break;
+      case OI_ANNUAL:
+        *f << "year" << FIELD_SEPARATOR;
+        break;
+      case OI_DECADAL:
+        // TODO
+        break;
+      default:
+        std::logic_error(
+            "Fauna::TextTableWriter::write_datapoint() Output time interval is "
+            "not implemented.");
+    }
+    *f << "agg_unit" << FIELD_SEPARATOR;
+  }
+
+  // Per HFT Tables
+  for (const auto i : datapoint.data.hft_data){
+    const std::string& hft_name = i.first->name;
+
+    if (mass_density_per_hft.is_open())
+      mass_density_per_hft << hft_name << FIELD_SEPARATOR;
+  }
+
+  for (auto& f : file_streams)
+    *f << std::endl;
+}
--- src/Fauna/Output/text_table_writer.h
+++ src/Fauna/Output/text_table_writer.h
@@ -1,6 +1,8 @@
 #ifndef TEXT_TABLE_WRITER_H
 #define TEXT_TABLE_WRITER_H
 
+#include <fstream>
+#include <vector>
 #include "output_writer_interface.h"
 #include "parameters.h"
 
@@ -15,6 +17,10 @@ namespace Output {
  * \ref Parameters::TextTableWriterOptions.
  * All files are created in a directory specified by
  * \ref Parameters::TextTableWriterOptions::output_directory.
+ *
+ * Per-HFT tables have one column per HFT:
+ * - `mass_density_per_hft`
+ *
  */
 class TextTableWriter : public OutputWriterInterface {
  public:
@@ -30,12 +36,25 @@ class TextTableWriter : public OutputWriterInterface {
   /// Append spatially & temporally aggregated output data to table files.
   /**
    * \param datapoint The output data to write.
+   *
+   * \warning `datapoint` must not change its structure between calls. For
+   * example, the list of HFTs must not change. This is because the column
+   * captions are written in the first call to this function. In subsequent
+   * calls there is no check whether the data tuples match the column captions.
+   *
    * \throw std::invalid_argument If an HFT name contains whitespaces or the
    * \ref FIELD_SEPARATOR.
+   *
    * \throw std::invalid_argument If `datapoint.combined_data.datapoint_count`
    * is zero.
+   *
    * \throw std::invalid_argument If `datapoint.interval` does not match the
    * given \ref OutputInterval.
+   *
+   * \throw std::invalid_argument If `datapoint.aggregation_unit` contains
+   * \ref FIELD_SEPARATOR
+   *
+   * \throw std::logic_error If the \ref OutputInterval is not implemented.
    */
   virtual void write_datapoint(const Datapoint& datapoint);
 
@@ -46,8 +65,22 @@ class TextTableWriter : public OutputWriterInterface {
   static const char* FILE_EXTENSION;
 
  private:
+  /// Write the first line in the output files: column headers
+  /**
+   * \param datapoint Any output data with the same structure (e.g. list of
+   * HFTs) as following output data points will have.
+   * \throw std::logic_error If the \ref OutputInterval is not implemented.
+   */
+  void write_captions(const Datapoint& datapoint);
+
+  bool captions_written = false;
+  std::vector<std::ofstream*> file_streams;  // only selected ones
   const OutputInterval interval;
   const Parameters::TextTableWriterOptions options;
+
+  /** @{ \name File Streams */
+  std::ofstream mass_density_per_hft;
+  /** @} */  // File Streams
 };
 }  // namespace Output
 }  // namespace Fauna
