31758480876a83bff72b7ed99c33275d324b9ff3
Wolfgang Traylor <wolfgang.traylor@senckenberg.de>
2019-09-16 17:23:46 +0200

    Create stub for unit test of InsfileReader

--- src/Fauna/insfile_reader.test.cpp
+++ src/Fauna/insfile_reader.test.cpp
@@ -6,6 +6,13 @@
  */
 #include "catch.hpp"
 #include "insfile_reader.h"
+#include "fileystem.h"
 using namespace Fauna;
 
-TEST_CASE("Fauna::InsfileReader") {}
+TEST_CASE("Fauna::InsfileReader") {
+  // The example instruction file has been copied to the build directory by
+  // CMake.
+  static const std::string INSFILE = "megafauna.toml";
+
+  REQUIRE(file_exists(INSFILE));
+}
